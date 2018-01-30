ef6e9463af695ff5c1b160e14dc8b2475254360a
SkynetIsACat <n.schuetz@students.unibe.ch>
2018-01-30 18:50:33 +0100

    recommit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cat Login.js PryvEventLoader.js Subject.js GenericDataModel.js LatestEventData.js UsernameLatestEventData.js CategoryEventData.js UsernameCategoryEventData.js  NavHeaderView.js EventView.js CategoryView.js LatestEventView.js AlertView.js LoadingView.js MainController.js pryv.js Main.js > ../Production_DashboardOltenQuestionnaires/bundled.js
+cat Login.js PryvEventLoader.js Subject.js GenericDataModel.js LatestEventData.js UsernameLatestEventData.js CategoryEventData.js UsernameCategoryEventData.js  NavHeaderView.js EventView.js CategoryView.js LatestEventView.js AlertView.js LoadingView.js Options.js OptionView.js MainController.js pryv.js Main.js > ../Production_DashboardOltenQuestionnaires/bundled.js
 #cp *.html ../Production_DashboardOltenQuestionnaires/
 #cp *.css ../Production_DashboardOltenQuestionnaires/
 cd ../Production_DashboardOltenQuestionnaires/
